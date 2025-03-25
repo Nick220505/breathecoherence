57d27d7b8f514a0f318f860204d633a3e049ee48
Nick220505 <juannicolaspardo@gmail.com>
2025-03-24 20:58:34 -0500

    Year for LICENSE changed to 2025

---
+++ src/features/products/controller.ts
@@ -0,0 +1,97 @@
+"use server";
+
+import { ActionState } from "@/lib/types/action";
+import { FormState } from "@/lib/types/form";
+import { Product } from "@prisma/client";
+import { revalidateTag } from "next/cache";
+import { productSchema } from "./schema";
+import { productService } from "./service";
+import { unstable_cache } from "next/cache";
+import { notFound } from "next/navigation";
+
+export const getAllProducts = unstable_cache(
+  productService.getAll,
+  ["products"],
+  {
+    revalidate: 3600,
+    tags: ["products"],
+  },
+);
+
+export const getProductById = unstable_cache(
+  async (id: string) => {
+    try {
+      return await productService.getById(id);
+    } catch (error) {
+      if (error instanceof Error && error.message === "Product not found") {
+        notFound();
+      }
+      throw error;
+    }
+  },
+  ["product"],
+  { revalidate: 3600, tags: ["products", "product"] },
+);
+
+export async function createOrUpdateProduct(
+  _prevState: FormState,
+  formData: FormData,
+): Promise<FormState<Product>> {
+  const rawData = Object.fromEntries(formData.entries());
+  const { success, data, error } = productSchema.safeParse(rawData);
+
+  if (!success) {
+    return {
+      errors: error.flatten().fieldErrors,
+      message: "Please fill in all required fields and ensure they are valid",
+      success: false,
+    };
+  }
+
+  try {
+    const { id, ...productData } = data;
+
+    if (id) {
+      const data = await productService.update(id, productData);
+      revalidateTag("products");
+      revalidateTag("product");
+      return {
+        errors: {},
+        message: "Product updated successfully",
+        success: true,
+        data,
+      };
+    } else {
+      const data = await productService.create(productData);
+      revalidateTag("products");
+      return {
+        errors: {},
+        message: "Product created successfully",
+        success: true,
+        data,
+      };
+    }
+  } catch (error) {
+    return {
+      errors: {},
+      message:
+        error instanceof Error ? error.message : "Failed to save product",
+      success: false,
+    };
+  }
+}
+
+export async function deleteProduct(id: string): Promise<ActionState<Product>> {
+  try {
+    const data = await productService.delete(id);
+    revalidateTag("products");
+    revalidateTag("product");
+    return { success: true, message: "Product deleted successfully", data };
+  } catch (error) {
+    return {
+      success: false,
+      message:
+        error instanceof Error ? error.message : "Failed to delete product",
+    };
+  }
+}
