4b236caf4ce5ec9f26bc85ee51ec121d8945155c
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-05-25 14:35:19 -0500

    chore(prisma): update import paths and enhance user model
    Refactored import paths for the Prisma client across multiple files to point to the new generated location. Updated the user model in the authentication module to include additional fields (id, email, name) for better user management. Adjusted the handling of optional fields to ensure safer defaults. This improves code organization and enhances the user authentication process.

--- src/features/products/repository.ts
+++ src/features/products/repository.ts
@@ -1,27 +1,26 @@
 import prisma from '@/lib/prisma';
-
-import { Product } from '../../app/generated/prisma';
+import { Product } from '@/prisma/generated';
 
 import { ProductFormData } from './schema';
 
 export const productRepository = {
   async getAll(): Promise<Product[]> {
-    return prisma.product.findMany({ orderBy: { createdAt: 'desc' } });
+    return await prisma.product.findMany({ orderBy: { createdAt: 'desc' } });
   },
 
   async getById(id: string): Promise<Product | null> {
-    return prisma.product.findUnique({ where: { id } });
+    return await prisma.product.findUnique({ where: { id } });
   },
 
   async create(data: ProductFormData): Promise<Product> {
-    return prisma.product.create({ data });
+    return await prisma.product.create({ data });
   },
 
   async update(id: string, data: ProductFormData): Promise<Product> {
-    return prisma.product.update({ where: { id }, data });
+    return await prisma.product.update({ where: { id }, data });
   },
 
   async delete(id: string): Promise<Product> {
-    return prisma.product.delete({ where: { id } });
+    return await prisma.product.delete({ where: { id } });
   },
 };
--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,6 +1,6 @@
 import { hash } from 'bcryptjs';
 
-import { PrismaClient } from '../app/generated/prisma';
+import { PrismaClient } from './generated';
 
 const prisma = new PrismaClient();
 
