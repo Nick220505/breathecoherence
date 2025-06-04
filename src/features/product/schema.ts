3e963ac46bf7cd537f1016de6669d1904d1f76aa
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-06-03 22:49:05 -0500

    Refactor: Update product schema and error handling in forms
    - Replaced getProductSchema with a direct productSchema definition, simplifying the schema structure and enhancing clarity.
    - Improved error handling in the ProductForm and ProductTableRow components by implementing localized error messages for validation issues, ensuring a better user experience.
    - Adjusted imports across components to reflect the new schema structure, promoting consistency and maintainability in the codebase.

--- src/features/product/schema.ts
+++ src/features/product/schema.ts
@@ -1,27 +1,21 @@
 import { ProductType } from '@prisma/client';
-import { getTranslations } from 'next-intl/server';
 import { z } from 'zod';
 
-export const getProductSchema = (
-  t: Awaited<ReturnType<typeof getTranslations<'ProductSchema'>>>,
-) =>
-  z.object({
-    id: z.string().optional(),
-    name: z.string().min(3, t('nameMin')),
-    description: z.string().min(10, t('descriptionMin')),
-    type: z.nativeEnum(ProductType, {
-      errorMap: () => ({ message: t('typeInvalid') }),
-    }),
-    price: z.coerce.number().min(0, t('priceMin')),
-    stock: z.coerce.number().int(t('stockInt')).min(0, t('stockMin')),
-    imageBase64: z
-      .union([
-        z.string().startsWith('data:image/', { message: t('imageInvalid') }),
-        z.string().max(0),
-        z.null(),
-        z.undefined(),
-      ])
-      .optional(),
-  });
+export const productSchema = z.object({
+  id: z.string().optional(),
+  name: z.string().min(3),
+  description: z.string().min(10),
+  type: z.nativeEnum(ProductType),
+  price: z.coerce.number().min(0),
+  stock: z.coerce.number().int().min(0),
+  imageBase64: z
+    .union([
+      z.string().startsWith('data:image/'),
+      z.string().max(0),
+      z.null(),
+      z.undefined(),
+    ])
+    .optional(),
+});
 
-export type ProductFormData = z.infer<ReturnType<typeof getProductSchema>>;
+export type ProductFormData = z.infer<typeof productSchema>;
