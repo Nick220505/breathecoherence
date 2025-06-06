2e3c093cedc1827cfb23580c3d8b099c86f3de99
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-06-06 14:31:14 -0500

    Refactor: Simplify product page parameter handling
    - Removed the generateStaticParams function to streamline product page logic.
    - Updated parameter extraction to use destructuring for improved readability.
    - Retained the getProductById action for fetching product details based on the provided ID.

--- src/app/[locale]/(shop)/store/product/[id]/page.tsx
+++ src/app/[locale]/(shop)/store/product/[id]/page.tsx
@@ -1,23 +1,15 @@
 import { notFound } from 'next/navigation';
 
-import { getAllProducts, getProductById } from '@/features/product/actions';
+import { getProductById } from '@/features/product/actions';
 
 import { ProductDetails } from './components/product-details';
-
-export async function generateStaticParams() {
-  const products = await getAllProducts();
-
-  return products.map((product) => ({ id: product.id }));
-}
-
-export const dynamicParams = true;
 
 export default async function ProductPage({
   params,
 }: Readonly<{
   params: Promise<{ id: string }>;
 }>) {
-  const id = (await params).id;
+  const { id } = await params;
   const product = await getProductById(id);
   if (!product) notFound();
   return <ProductDetails product={product} />;
