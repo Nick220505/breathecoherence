5382e70856a61c93149154ace04c8eb7ff667ff0
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-06-08 22:12:07 -0500

    Refactor productService to utilize productRepository for database operations, streamline translation handling during product creation and updates, and enhance getTranslatedProduct function for improved readability and performance.

---
+++ src/features/product/repository.ts
@@ -0,0 +1,74 @@
+import { Prisma } from '@prisma/client';
+
+import prisma from '@/lib/prisma';
+
+export const productRepository = {
+  findMany: () => {
+    return prisma.product.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+  },
+
+  findById: (id: string) => {
+    return prisma.product.findUnique({ where: { id } });
+  },
+
+  findTranslation: (productId: string, locale: string) => {
+    return prisma.productTranslation.findUnique({
+      where: {
+        productId_locale: {
+          productId,
+          locale,
+        },
+      },
+    });
+  },
+
+  create(
+    productData: Prisma.ProductCreateInput,
+    translations: { locale: string; name: string; description: string }[],
+  ) {
+    return prisma.$transaction(async (tx) => {
+      const newProduct = await tx.product.create({ data: productData });
+      for (const t of translations) {
+        await tx.productTranslation.create({
+          data: { ...t, productId: newProduct.id },
+        });
+      }
+      return newProduct;
+    });
+  },
+
+  update(
+    id: string,
+    productData: Prisma.ProductUpdateInput,
+    translations: { locale: string; name: string; description: string }[],
+  ) {
+    return prisma.$transaction(async (tx) => {
+      const updatedProduct = await tx.product.update({
+        where: { id },
+        data: productData,
+      });
+
+      for (const t of translations) {
+        await tx.productTranslation.upsert({
+          where: {
+            productId_locale: {
+              productId: updatedProduct.id,
+              locale: t.locale,
+            },
+          },
+          update: { name: t.name, description: t.description },
+          create: { ...t, productId: updatedProduct.id },
+        });
+      }
+      return updatedProduct;
+    });
+  },
+
+  delete: (id: string) => {
+    return prisma.product.delete({ where: { id } });
+  },
+};
