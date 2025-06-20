23a1c9561eadffbc80d0487940b8093801e6bc8b
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-06-20 13:55:51 -0500

    Refactor admin dashboard layout by adjusting Sidebar and DashboardLayout components for improved responsiveness and structure. Updated Sidebar to use fixed positioning and modified layout styles to enhance user experience across different screen sizes.

--- src/app/[locale]/(admin)/dashboard/components/sidebar.tsx
+++ src/app/[locale]/(admin)/dashboard/components/sidebar.tsx
@@ -61,7 +61,7 @@ function SidebarHeader() {
 
 export function Sidebar() {
   return (
-    <div className="bg-muted/40 hidden border-r md:block">
+    <div className="bg-muted/40 hidden border-r md:fixed md:top-16 md:left-0 md:block md:h-[calc(100vh-4rem)] md:w-[220px] lg:w-[280px]">
       <div className="flex h-full max-h-screen flex-col gap-2">
         <SidebarHeader />
         <div className="flex-1">
--- src/app/[locale]/(admin)/dashboard/layout.tsx
+++ src/app/[locale]/(admin)/dashboard/layout.tsx
@@ -10,9 +10,9 @@ export default function DashboardLayout({
   children,
 }: Readonly<DashboardLayoutProps>) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="min-h-screen w-full">
       <Sidebar />
-      <div className="flex flex-col">
+      <div className="flex flex-col md:ml-[220px] lg:ml-[280px]">
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           {children}
         </main>
