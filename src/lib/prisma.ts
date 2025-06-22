4b4d681cd05dc27cf60e80b2deca131251663a92
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-06-22 12:05:08 -0500

    Enhance Prisma client initialization with logging configuration based on environment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,14 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
