d6c26827764c6b0db7524bf4a4f50948d65f7e8b
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-06-23 14:18:18 -0500

    Enhance Toaster component in LocaleLayout by adding closeButton and richColors props for improved user interaction and visual appeal.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -39,7 +39,7 @@ export default async function LocaleLayout({
             <Footer />
           </div>
           <div className="fixed right-4 bottom-4 z-50">
-            <Toaster />
+            <Toaster closeButton richColors />
           </div>
           <ChatBot />
         </Providers>
