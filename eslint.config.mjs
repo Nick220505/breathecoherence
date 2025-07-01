3bf928732cce35f7c9e47aa8c81129b94953b68b
Mia220505 <mchughmia832@gmail.com>
2025-07-01 02:36:22 -0500

    refactor: Remove unused eslint-plugin-simple-import-sort from dependencies and configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,10 +9,6 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    plugins: { 'simple-import-sort': simpleImportSort },
-    rules: { 'simple-import-sort/imports': 'error' },
-  },
 ];
 
 export default eslintConfig;
