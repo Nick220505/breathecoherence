70b0eea1df831de7192dae93b76cdee5d73cf341
Nick220505 <129890121+Nick220505@users.noreply.github.com>
2025-07-07 15:41:00 -0500

    feat: remove 'DELIVERED' and 'CANCELLED' statuses from OrderStatus enum

--- src/prisma/migrations/20250707201553_remove_unused_order_statuses/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The values [DELIVERED,CANCELLED] on the enum `OrderStatus` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterEnum
-ALTER TYPE "OrderStatus"DROP VALUE 'DELIVERED';
-ALTER TYPE "OrderStatus"DROP VALUE 'CANCELLED';
